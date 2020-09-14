faed8f0a0c65a556c157e17704172f0900bc5067
Patrick Tappe <pattappe@gmail.com>
2020-09-14 12:25:40 -0400

    3.10

---
+++ build/precache-manifest.6d30b41df6964ffc725f48cbcfef198c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c064d3343576eda732ffe8757c7a9cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e35caac2af3ff4cd03",
+    "url": "/static/css/main.9f32ee99.chunk.css"
+  },
+  {
+    "revision": "6fa96f4e77e54634d75a",
+    "url": "/static/js/2.89f8abb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89f8abb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e35caac2af3ff4cd03",
+    "url": "/static/js/main.a3913432.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
